efcf5a8f68fb5d4e4d700258742bfe2e81d16591
wee08 <manutnithyasean@gmail.com>
2026-01-06 19:12:36 +0700

    update emial key

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -9,7 +9,7 @@ const ContactUs = () => {
     event.preventDefault();
     const formData = new FormData(event.target);
 
-    formData.append("access_key", "32f6b580-c2f2-49bd-9d7d-5156ac7913ab ");
+    formData.append("access_key", "32f6b580-c2f2-49bd-9d7d-5156ac7913ab");
 
     try {
       const response = await fetch("https://api.web3forms.com/submit", {
