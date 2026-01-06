b7edf0620d73e108caea4511ec2673451a6d9fbf
wee08 <manutnithyasean@gmail.com>
2026-01-06 18:51:46 +0700

    update tailwindcss

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { Route } from "react-router";
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import "./index.css"; // <-- make sure Tailwind is imported here
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/Agency-ai/">
+      {" "}
+      {/* important for GitHub Pages */}
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
