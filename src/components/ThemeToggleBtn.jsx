6e1fa40365fc811c7ddbd06fe5f1872b7c8351a9
manut-tech <manutnithyasean@gmail.com>
2026-01-06 18:18:07 +0700

    first commit

---
+++ src/components/ThemeToggleBtn.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+import assets from "../assets/assets";
+const ThemeToggleBtn = ({ theme, setTheme }) => {
+  useEffect(() => {
+    const prefersDarkMode = window.matchMedia(
+      "(prefers-color-scheme: dark"
+    ).matches;
+    setTheme(theme || (prefersDarkMode ? "dark" : "light"));
+  }, []);
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  return (
+    <>
+      <button>
+        {theme === "dark" ? (
+          <img
+            src={assets.sun_icon}
+            alt=""
+            onClick={() => setTheme("light")}
+            className="
+          size-8.5 p-1.5 border border-gray-500 rounded-full"
+          />
+        ) : (
+          <img
+            src={assets.moon_icon}
+            alt=""
+            onClick={() => setTheme("dark")}
+            className="
+          size-8.5 p-1.5 border border-gray-500 rounded-full"
+          />
+        )}
+      </button>
+    </>
+  );
+};
+
+export default ThemeToggleBtn;
